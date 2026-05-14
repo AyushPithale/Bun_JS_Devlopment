14d3c1b3868f2c631aaa26971fd0e6bdf393e24d
Ayush Pithale <ayupithale619@gmail.com>
2026-05-15 00:34:56 +0530

    added some basics

--- Concepts/features/fs.ts
+++ Concepts/features/fs.ts
@@ -2,4 +2,25 @@ import type { BunFile } from "bun";
 
 async function fileSystemoperations() {
    //  read  a file 
-}
+   const file : BunFile = Bun.file('read.txt')
+   console.log(file.text)
+   console.log(file.size);
+    console.log(file.type, "type"); 
+
+    const exracttext = await file.text()
+      console.log(exracttext, "extracted text");
+
+    const unintBuffer = await file.arrayBuffer()
+    console.log(unintBuffer, "unint buffer");
+     
+
+    const unintArray = await file.bytes()
+    console.log(unintArray, "unint array");
+
+    const content = "hello" 
+
+    const writenFile = await Bun.write("write.txt", content)
+    console.log(writenFile, "writen file");
+   }
+
+fileSystemoperations()
